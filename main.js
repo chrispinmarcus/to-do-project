d74de3ec5e2813517851d9240ece7245517ff04d
chrispinmarcus <jeanchrispin53@gmail.com>
2025-06-08 12:23:45 +0200

    this is for more evolution

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-let addToDoButton = document.getElementById("addToDo")
+let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
-addToDoButton.addEventListener('click',()=>{
-    var paragraph = document.createElement("p");
-    toDoContainer.appendChild(paragraph);
-    paragraph.innerText= inputField.value;
-    inputField.value ='';
+addToDoButton.addEventListener("click",()=>{
+if (inputField.value.trim() ===''){
+        alert('please this should not be empty!');
+            return;
+        }
+        let paragraph = document.createElement('p');
+        paragraph.innerText = inputField.value;
+        toDoContainer.appendChild(paragraph);
+        inputField.value ='';
 
-    paragraph.addEventListener('click', () => {
-    paragraph.style.textDecoration = "line-through";
-});
-
-    paragraph.addEventListener('dblclick',()=>{
-        paragraph.remove();
-    })
+        paragraph.addEventListener('click',()=>{
+            paragraph.style.textDecoration ='line-through';
+        })
+        paragraph.addEventListener('dblclick',()=>{
+            toDoContainer.removeChild(paragraph);
+        })
 
 })
+    
+
+       
